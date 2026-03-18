d2336f99b308eae126d78f0e0c8927f4bbfa752d
parkhyunjun <lanlaria@hufs.ac.kr>
2026-03-19 01:41:19 +0900

    useìWisSayingController

--- src/main/java/com/domain/systemcontroller/SystemController.java
+++ src/main/java/com/domain/systemcontroller/SystemController.java
@@ -1,4 +1,4 @@
-package com.domain;
+package com.domain.systemcontroller;
 
 public class SystemController {
     public void exit(){
--- src/main/java/com/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/domain/wiseSaying/entity/WiseSaying.java
@@ -1,4 +1,4 @@
-package com.domain;
+package com.domain.wiseSaying.entity;
 public class WiseSaying {
 
     private int id;
